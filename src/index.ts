b52ec5686f7f1fd7d8b7b05099c993971f9010a4
Victor Lamers <57627630+LamersV@users.noreply.github.com>
2024-12-18 15:18:56 -0300

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ export interface UnzipOptions {
   recursive?: boolean;
   overwrite?: boolean;
   errorOnExist?: boolean;
+  filenameFormatter?: (filename: string, buffer?: Buffer) => string;
 }
 
 export { unzip } from "./modules/zip";
